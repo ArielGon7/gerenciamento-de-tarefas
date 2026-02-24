9c69f5449117bceb06570501078164834ee13b2f
ArielGon7 <arielgomessantos7@gmail.com>
2026-02-23 23:08:50 -0300

    SecurityConfig adicionado para armazenar @Bean.

---
+++ src/main/java/com/devariel/gerenciamento_de_tarefas/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.devariel.gerenciamento_de_tarefas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
