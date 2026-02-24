73feb01532ea9f82e1f9ffb76068ae9963c2d473
ArielGon7 <arielgomessantos7@gmail.com>
2026-02-23 23:08:04 -0300

    UserService implementado e metodos criados.

---
+++ src/main/java/com/devariel/gerenciamento_de_tarefas/service/UserService.java
@@ -0,0 +1,27 @@
+package com.devariel.gerenciamento_de_tarefas.service;
+
+import com.devariel.gerenciamento_de_tarefas.model.User;
+import com.devariel.gerenciamento_de_tarefas.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    
+    public User userSave(String username, String password){
+        String encryptedPassword = passwordEncoder.encode(password);
+        User user = new User(username, encryptedPassword);
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findByUsername(String username){
+        return userRepository.findByUsername(username);
+    }
+}
