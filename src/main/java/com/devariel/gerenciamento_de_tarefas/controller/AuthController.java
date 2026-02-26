c18f83fcb2b1c49788bb0c5797e22c901f0074b8
ArielGon7 <arielgomessantos7@gmail.com>
2026-02-26 00:02:39 -0300

    Método login implementado para o AuthController.

--- src/main/java/com/devariel/gerenciamento_de_tarefas/controller/AuthController.java
+++ src/main/java/com/devariel/gerenciamento_de_tarefas/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.devariel.gerenciamento_de_tarefas.controller;
 
 import com.devariel.gerenciamento_de_tarefas.model.User;
+import com.devariel.gerenciamento_de_tarefas.security.JwtUtil;
 import com.devariel.gerenciamento_de_tarefas.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,9 +20,19 @@ public class AuthController {
 
     private final UserService userService;
 
-    @PostMapping
+    @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody Map<String, String> request) {
         User user = userService.userSave(request.get("username"), "password");
         return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Map<String, String> request){
+        Optional<User> user = userService.findByUsername(request.get("username"));
+        if(user.isPresent() && user.get().getPassword().equals(request.get("password"))){
+            String token = JwtUtil.generateToken(user.get().getUsername());
+            return ResponseEntity.ok(Map.of("token", token));
+        }
+        return ResponseEntity.status(401).body("invalid field");
     }
 }
