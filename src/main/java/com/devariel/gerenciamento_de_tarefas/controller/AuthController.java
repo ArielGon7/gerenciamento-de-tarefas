5f3210f05b9ecea273ff651827648c022e308b3b
ArielGon7 <arielgomessantos7@gmail.com>
2026-02-25 23:40:06 -0300

    Método register implementado para o AuthController.

---
+++ src/main/java/com/devariel/gerenciamento_de_tarefas/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.devariel.gerenciamento_de_tarefas.controller;
+
+import com.devariel.gerenciamento_de_tarefas.model.User;
+import com.devariel.gerenciamento_de_tarefas.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<?> register(@RequestBody Map<String, String> request) {
+        User user = userService.userSave(request.get("username"), "password");
+        return ResponseEntity.ok(user);
+    }
+}
